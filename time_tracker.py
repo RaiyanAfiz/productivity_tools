e28d10a944ef1fea796f798406b163ed8294b724
Raiyan Afiz <raiyan.afiz@gmail.com>
2025-02-13 17:35:01 -0500

    Update timetracker

--- time_tracker.py
+++ time_tracker.py
@@ -19,21 +19,28 @@ hours, remainder = divmod(timeSec, 3600)
 minutes, seconds = divmod(remainder, 60)
 timeSecFormat = '{:02}h {:02}m'.format(int(hours), int(minutes))
 
+#Converting to 12 hour format 
 date = start_dt.strftime("%d/%m/%Y")
 s_time = start_dt.strftime("%I:%M %p")
 e_time = end_dt.strftime("%I:%M %p")
 
 lastDateEntry = ""
 
-with open('time_tracker.csv', 'r', newline='') as file:
-    last_line = file.readlines()[-1]
-    lastDateEntry = last_line.split(",")[0]
+# !!!!! Reading file. Fix this later !!!!! 
+try:
+    with open('time_tracker.csv', 'r', newline='') as file:
+        last_line = file.readlines()[-1]
+        lastDateEntry = last_line.split(",")[0]
+except FileNotFoundError:
+    print('File not found. Creating new file.')
 
+# Update file with new task
 with open('time_tracker.csv', 'a', newline='') as file:
     writer = csv.writer(file, delimiter=',')
-    if lastDateEntry != str(date):
+    if lastDateEntry != str(date) and lastDateEntry != "":
         writer.writerow('')
     writer.writerow([date, taskName, s_time, e_time, timeSecFormat])
 
-
+# User message
 print("Updated.")
+input("Press enter to close program.")
